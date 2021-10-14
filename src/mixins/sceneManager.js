4f33f1c97767e32396a26ff742406ce360720a66
remi <rmichel@eukles.com>
2021-10-15 00:13:26 +0200

    build(babylon-js): merge 2 .glb file to make clones

--- src/mixins/sceneManager.js
+++ src/mixins/sceneManager.js
@@ -1,5 +1,7 @@
 import {BABYLON} from 'vue-babylonjs';
 
+const MAX_ASSET_TO_LOAD = 2;
+
 export default {
 
     data() {
@@ -67,49 +69,25 @@ export default {
                 this.scene,
                 this.success
             );
-
-            // to translate meshes
-
-            // BABYLON.SceneLoader.ImportMesh(
-            //     "",
-            //     'assets/',
-            //     "submeshes_test.glb",
-            //     this.scene,
-            //     (meshes, particleSystems, skeletons, animationGroups) => {
-            //         console.log(meshes);
-            //         let root = meshes[0];
-            //         root.position.x = -5
-            //         this.mesh_list.set('root', meshes[0]);
-            //         this.mesh_list.set('left_ear', meshes[3]);
-            //     }
-            // );
-            // BABYLON.SceneLoader.ImportMesh(
-            //     "",
-            //     'assets/',
-            //     "submeshes_test_2.glb",
-            //     this.scene,
-            //     (meshes, particleSystems, skeletons, animationGroups) => {
-            //
-            //         let root = this.mesh_list.get('root');
-            //         let left_ear = this.mesh_list.get('left_ear');
-            //         left_ear.position.y = 2
-            //         root.removeChild(left_ear)
-            //         let bleu_left_ear = meshes[3];
-            //
-            //         bleu_left_ear.translate(new BABYLON.Vector3(-5, 0, 0), 1, BABYLON.Space.WORLD)
-            //
-            //         root.addChild(bleu_left_ear)
-            //         root.position.y = -1
-            //         root.position.x = -4
-            //         root.rotation = new BABYLON.Vector3(0, (Math.PI/2)*4, 0)
-            //
-            //     }
-            // );
+            BABYLON.SceneLoader.ImportMesh(
+                "",
+                'assets/',
+                "submeshes_left_arm_2.glb",
+                this.scene,
+                (meshes, particleSystems, skeletons, animationGroups) => {
+                    this.mesh_list.set('blue_left_arm', meshes[2])
+                    this.mesh_list.set('blue_2be1_body', meshes[1])
+                    this.mesh_list.set('blue_2be1', meshes[0])
+                    this.skeleton_list.set('blue_2be1', skeletons[0])
+                    this.animation_list.set('blue_2be1', animationGroups)
+                    this.asset_loaded++;
+                }
+            );
         },
 
         success(meshes, particleSystems, skeletons, animationGroups) {
 
-            this.mesh_list.set(meshes[2].name, meshes[2])
+            this.mesh_list.set('left_arm', meshes[2])
             this.mesh_list.set('2be1', meshes[0])
 
             this.skeleton_list.set('2be1', skeletons[0])
@@ -118,38 +96,35 @@ export default {
         },
 
         clone_mesh(name, root_mesh, root_skeleton, root_animation_group_list) {
-
+            // maps use for unlink bones and animations
             let conversionMap = {}
             let storeMap = {}
             let clone_mesh = root_mesh.instantiateHierarchy(null, {doNotInstantiate: true}, (source, clone) => {
                 conversionMap[source.uniqueId] = clone.uniqueId
                 storeMap[clone.uniqueId] = clone
             });
-
+            // clone the skeleton and attach it to clone_skeleton's descendants with sub-meshes
             let clone_skeleton = root_skeleton.clone("clone_skeleton");
-
             clone_skeleton.overrideMesh = clone_mesh;
-
             let descendants = clone_mesh.getDescendants(false);
             for (let i = 0; i < descendants.length; i++) {
                 if (descendants[i].subMeshes) {
                     descendants[i].skeleton = clone_skeleton;
                 }
             }
-
+            // convert the _linkedTransformNode of clone_skeleton's bones
             for (let bone of clone_skeleton.bones) {
                 if (bone._linkedTransformNode) {
                     bone._linkedTransformNode = storeMap[conversionMap[bone._linkedTransformNode.uniqueId]]
                 }
             }
+            // clone all animations group and change the target to the clone_mesh if it already had one
             let clone_animation_group_list = [];
             root_animation_group_list.forEach((animation_group) => {
-
                 let clone_animation_group = animation_group.clone(animation_group.name + '-clone', (oldTarget) => {
                     let newTarget = storeMap[conversionMap[oldTarget.uniqueId]]
                     return newTarget || oldTarget
                 })
-
                 clone_animation_group_list.push(clone_animation_group)
             });
 
@@ -195,25 +170,49 @@ export default {
     },
     watch: {
         asset_loaded() {
-            if (this.asset_loaded === 1){
-                if (this.asset_loaded) {
-                    // let's make the 2b3 : 2 clones of 2be1 !
-                    let rootMesh = this.mesh_list.get('2be1');
-                    let rootSkeleton = this.skeleton_list.get('2be1');
-                    let rootAnimationGroups = this.animation_list.get('2be1');
+            if (this.asset_loaded === MAX_ASSET_TO_LOAD) {
+                // let's make the 2b3 : 2 clones of 2be1 !
+                let rootMesh = this.mesh_list.get('2be1');
+                let blue_rootMesh = this.mesh_list.get('blue_2be1');
+                let rootSkeleton = this.skeleton_list.get('2be1');
+                let rootAnimationGroups = this.animation_list.get('2be1');
 
-                    rootMesh.scaling = new BABYLON.Vector3(0.025, 0.025, .025);
-                    let posX = 5;
-                    for (let i = 2; i < 4; i++) {
-                        this.clone_mesh('2be' + i, rootMesh, rootSkeleton, rootAnimationGroups)
-                        let clone = this.mesh_list.get('2be' + i);
-                        clone.position.x = posX
-                        posX += 5;
-                    }
+                rootMesh.scaling = new BABYLON.Vector3(0.025, 0.025, .025);
+                blue_rootMesh.scaling = new BABYLON.Vector3(0.025, 0.025, .025);
+                blue_rootMesh.position.x = -5
 
-                    let camera = this.camera_list.get("FollowCam");
-                    camera.lockedTarget = this.mesh_list.get('2be2');
+                // add the blue left arm for the other .glb file
+                let left_arm = this.mesh_list.get('left_arm');
+                let blue_left_arm = this.mesh_list.get('blue_left_arm');
+                // remove the original from the parent to have a clone in 0 0 0 coordinate (optional)
+                blue_rootMesh.removeChild(blue_left_arm)
+                let clone_blue_left_arm = blue_left_arm.clone('clone_blue_left_arm')
+                clone_blue_left_arm.position.y = -5;
+                // left_arm will be missing from the clones
+                rootMesh.removeChild(left_arm)
+                // add clone_blue_left_arm to the clones
+                rootMesh.addChild(clone_blue_left_arm)
+
+                // clean the scene, remove unused mesh
+                this.scene.removeMesh(blue_rootMesh)
+                this.scene.removeMesh(blue_left_arm)
+                this.scene.removeMesh(clone_blue_left_arm)
+                let blue_2be1_body = this.mesh_list.get('blue_2be1_body');
+                this.scene.removeMesh(blue_2be1_body)
+
+                // cloning
+                let posX = 5;
+                for (let i = 2; i < 4; i++) {
+                    this.clone_mesh('2be' + i, rootMesh, rootSkeleton, rootAnimationGroups)
+                    let clone = this.mesh_list.get('2be' + i);
+                    clone.position.x = posX
+                    posX += 5;
                 }
+
+                // focus
+                let camera = this.camera_list.get("FollowCam");
+                camera.lockedTarget = this.mesh_list.get('2be2');
+
             }
         }
     }
